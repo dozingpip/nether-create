2b90da36c771ae7f71f88c471b190361556cbd42
dozingpip <dozingpip@gmail.com>
2024-10-09 18:24:26 -0400

    removed pure daisy cobblestone recipe made sure recipes with input obsidian will now use the forge:obsidian tag

--- kubejs/server_scripts/remove_recipes.js
+++ kubejs/server_scripts/remove_recipes.js
@@ -3,5 +3,7 @@
 ServerEvents.recipes(event => {
     event.remove({output: 'minecraft:obsidian'})
     event.remove({id: 'botania:pure_daisy/obsidian'})
+    event.remove({id: 'botania:pure_daisy/cobblestone'})
     event.remove({type: 'create:fan_washing'})
+    event.replaceInput({input: "minecraft:obsidian"}, "minecraft:obsidian", "#forge:obsidian")
 })
